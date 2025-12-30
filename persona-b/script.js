5fba9f00e77fbfc25184fd7bf9c9c4aa2ce45aa7
HaruGraphics <harugraphicss@gmail.com>
2025-12-30 05:33:59 +0530

    Delete persona-b directory

--- persona-b/script.js
+++
@@ -1,9 +0,0 @@
-const bg = document.getElementById("bg");
-const items = document.querySelectorAll(".item");
-
-items.forEach(item => {
-  item.addEventListener("mouseenter", () => {
-    const img = item.getAttribute("data-bg");
-    bg.style.backgroundImage = `url(${img})`;
-  });
-});
